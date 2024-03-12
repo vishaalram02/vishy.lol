f8f21de50bdc791614432ee76050fb276be0e562
vishaalram02 <vishaalram02@gmail.com>
2024-03-12 13:51:20 -0400

    google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,6 +16,17 @@
 		crossorigin="anonymous"
 		referrerpolicy="no-referrer"
 	/>
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-C9RRXFBHB4"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+		gtag('config', 'G-C9RRXFBHB4');
+	</script>
 </svelte:head>
 
 <Header />
